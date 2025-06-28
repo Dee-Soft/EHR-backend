4cff0504e49f5051e919033ec165466753162e00
Sarfaraj84 <dee.dev.au@gmail.com>
2025-06-28 18:49:37 +1000

    Updated serverJS

--- server.js
+++ server.js
@@ -1,21 +1,38 @@
 require('dotenv').config();
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
 const connectDB = require('./config/db');
+
+//routes
 const userRoutes = require('./routes/userRoutes');
 const authRoutes = require('./routes/authRoutes');
 const patientRecordRoutes = require('./routes/patientRecordRoutes');
+const adminRoutes = require('./routes/adminRoutes');
 
 const app = express();
+
+// middleware
+app.use(helmet());
+app.use(cors());
+app.use(rateLimit({ 
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+}));
 app.use(express.json());
+
 connectDB();
 
+// api endpoints
 app.get('/', (req, res) => {
-  res.send('Electronic Health Record System');
+  res.send('Electronic Health Record System backend is running securely!');
 });
-
 app.use('/api/users', userRoutes);
 app.use('/api/patient-records', patientRecordRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/admin', adminRoutes);
 
+// start server
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
