a5efd08e183593e5a44c6f52764bf44e388bad2b
Sarfaraj84 <dee.dev.au@gmail.com>
2025-06-30 03:21:44 +1000

    Fixed userRoutes file which was causing problem running docker container

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const { registerUser, updateUser } = require('./controllers/userController');
-const { authMiddleware, requireRole } = require('./middlewares/authMiddleware');
+const { registerUser, updateUser } = require('../controllers/userController');
+const { authMiddleware, requireRole } = require('../middlewares/authMiddleware');
 const router = express.Router();
 
 router.post('/register', authMiddleware, requireRole('Employee', 'Manager', 'Admin'), registerUser);
